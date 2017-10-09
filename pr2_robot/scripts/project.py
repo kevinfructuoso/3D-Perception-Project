c0d2209f1cf6cbdb30d0f10a01266b0621a0b436
kevinfructuoso <keevinfructuoso@yahoo.com>
2017-10-09 00:45:51 -0700

    update statistical outlier tuning and clustering for actual project

--- pr2_robot/scripts/project.py
+++ pr2_robot/scripts/project.py
@@ -49,8 +49,6 @@ def send_to_yaml(yaml_filename, dict_list):
 # Callback function for your Point Cloud Subscriber
 def pcl_callback(pcl_msg):
 
-# Exercise-2 TODOs:
-
     # Convert ROS msg to PCL data
     pcl_msg = ros_to_pcl(pcl_msg)
 
@@ -58,10 +56,10 @@ def pcl_callback(pcl_msg):
     outlier_filter = pcl_msg.make_statistical_outlier_filter()
 
     # Set the number of neighboring points to analyze for any given point
-    outlier_filter.set_mean_k(50)
+    outlier_filter.set_mean_k(10)
 
     # Set threshold scale factor
-    x = 1.0
+    x = 0.25
 
     # Any point with a mean distance larger than global (mean distance+x*std_dev) will be considered outlier
     outlier_filter.set_std_dev_mul_thresh(x)
@@ -111,37 +109,90 @@ def pcl_callback(pcl_msg):
     inliers, coefficients = seg.segment()
 
     # Extract inliers (table top points) and outliers (table top object points)
-    extracted_inliers = cloud_filtered.extract(inliers, negative=False)
+    #extracted_inliers = cloud_filtered.extract(inliers, negative=False)
     extracted_outliers = cloud_filtered.extract(inliers, negative=True)
 
-    # TODO: Euclidean Clustering
+    # Euclidean Clustering
+    white_cloud = XYZRGB_to_XYZ(extracted_outliers)
+    tree = white_cloud.make_kdtree()
 
-    # TODO: Create Cluster-Mask Point Cloud to visualize each cluster separately
+    # Create a cluster extraction object
+    ec = white_cloud.make_EuclideanClusterExtraction()
+    # Set tolerances for cluser search
+    ec.set_ClusterTolerance(0.0075)
+    ec.set_MinClusterSize(400)
+    ec.set_MaxClusterSize(2000)
 
-    # TODO: Convert PCL data to ROS messages
+    # Search the k-d tree for clusters
+    ec.set_SearchMethod(tree)
+
+    # Extract indices for each of the discovered clusters
+    cluster_indices = ec.Extract()
+
+    # Create Cluster-Mask Point Cloud to visualize each cluster separately
+    # Assign a color corresponding to each segmented object in scene
+    cluster_color = get_color_list(len(cluster_indices))
+
+    color_cluster_point_list = []
+
+    for j, indices in enumerate(cluster_indices):
+        for i, indice in enumerate(indices):
+             color_cluster_point_list.append([white_cloud[indice][0],
+                                              white_cloud[indice][1],
+                                              white_cloud[indice][2],
+                                              rgb_to_float(cluster_color[j])])
+
+    #Create new cloud containing all clusters, each with unique color
+    cluster_cloud = pcl.PointCloud_PointXYZRGB()
+    cluster_cloud.from_list(color_cluster_point_list)
+
+    # Convert PCL data to ROS messages
     #ros_cloud_objects = pcl_to_ros(cloud_filtered)
     ros_cloud_objects = pcl_to_ros(extracted_outliers)
-    ros_cloud_table = pcl_to_ros(extracted_inliers)
+    #ros_cloud_table = pcl_to_ros(extracted_inliers)
+    ros_cluster_cloud = pcl_to_ros(cluster_cloud)
 
-    # TODO: Publish ROS messages
+    # Publish ROS messages
     pcl_objects_pub.publish(ros_cloud_objects)
-    pcl_table_pub.publish(ros_cloud_table)
-
-# Exercise-3 TODOs:
+    #pcl_table_pub.publish(ros_cloud_table)
+    pcl_cluster_pub.publish(ros_cluster_cloud)
 
     # Classify the clusters! (loop through each detected cluster one at a time)
+    detected_objects_labels = []
+    detected_objects = []
+
+    for index, pts_list in enumerate(cluster_indices):
 
         # Grab the points for the cluster
+        pcl_cluster = extracted_outliers.extract(pts_list)
+        ros_pcl_cluster = pcl_to_ros(pcl_cluster)
 
         # Compute the associated feature vector
+        # Extract histogram features
+        chists = compute_color_histograms(ros_pcl_cluster, using_hsv=True)
+        normals = get_normals(ros_pcl_cluster)
+        nhists = compute_normal_histograms(normals)
+        feature = np.concatenate((chists, nhists))
 
         # Make the prediction
+        #prediction = clf.predict(scaler.transform(feature.reshape(1,-1)))
+        #label = encoder.inverse_transform(prediction)[0]
+        #detected_objects_labels.append(label)
 
         # Publish a label into RViz
+        #label_pos = list(white_cloud[pts_list[0]])
+        #label_pos[2] += 0.4
+        #object_markers_pub.publish(make_label(label, label_pos, index))
 
         # Add the detected object to the list of detected objects.
+        #do = DetectedObject()
+        #do.label = label
+        #do.cloud = ros_pcl_cluster
+        #detected_objects.append(do)
 
     # Publish the list of detected objects
+    #rospy.loginfo('Detected {} objects: {}'.format(len(detected_objects_labels), detected_objects_labels))
+    #detected_objects_pub.publish(detected_objects)
 
     # Suggested location for where to invoke your pr2_mover() function within pcl_callback()
     # Could add some logic to determine whether or not your object detections are robust
@@ -192,16 +243,18 @@ def pr2_mover(object_list):
 
 if __name__ == '__main__':
 
-    # TODO: ROS node initialization
+    # ROS node initialization
     rospy.init_node('clustering', anonymous=True)
 
-    # TODO: Create Subscribers
+    # Create Subscribers
     pcl_sub = rospy.Subscriber("/pr2/world/points",pc2.PointCloud2, pcl_callback, queue_size=1)
 
-    # TODO: Create Publishers
+    # Create Publishers
     pcl_objects_pub = rospy.Publisher("/pcl_objects", PointCloud2, queue_size=1)
-    pcl_table_pub = rospy.Publisher("/pcl_table", PointCloud2, queue_size=1)
-
+    #pcl_table_pub = rospy.Publisher("/pcl_table", PointCloud2, queue_size=1)
+    pcl_cluster_pub = rospy.Publisher("/pcl_cluster", PointCloud2, queue_size=1)
+    object_markers_pub = rospy.Publisher("/object_markers", Marker, queue_size=1)
+    detected_objects_pub = rospy.Publisher("/detected_objects", DetectedObjectsArray, queue_size=1)
 
     # TODO: Load Model From disk
 
